be3d7b0c650bb28acd141473ce87d3afb64d66ea
DanielaGarciaK <danielatramp@gmail.com>
2021-05-17 16:17:16 +0200

    sett

---
+++ GMDProject/Assets/SCRIPTS/BackroundScroller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackroundScroller : MonoBehaviour
+{
+    public float speed = 0.5f;
+    private float offset;
+    private Material mat;
+
+    private void Start()
+    {
+        mat = GetComponent <Renderer>().material;    }
+    private void Update()
+    {
+        offset +=(Time.deltaTime*speed)/10f;
+        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
---
+++ GMDProject/Assets/SCRIPTS/enemie.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemie : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] enemyPrefabs;
+    public float speed = 10.0f;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (speed <= 0)
+        {
+            int randEnemy = Random.Range(0, enemyPrefabs.Length);
+            int randSpawPoint = Random.Range(0, spawnPoints.Length);
+
+            Instantiate(enemyPrefabs[randEnemy], spawnPoints[randSpawPoint].position, transform.rotation);
+        }
+        else
+        {
+            speed -= Time.deltaTime;
+        }
+    }
+}
